bd09ed3e6789f626b4be69b9a71e131900275b51
nonylene <cookie.speaker@gmail.com>
2014-06-10 19:29:09 +0900

    修正

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ var welcome = document.getElementById('welcome');
 welcome.addEventListener("click",function(){
 	alert('welcome');
 });
-var setfnc = function(){
-	alert(timersc.value + "びょう経ちました")
+var setFnc = function(){
+	alert(timerSc.value + "びょう経ちました")
 };
-var timersc = document.getElementById('timerMSec');
+var timerSc = document.getElementById('timerMSec');
 var timerBtn = document.getElementById('timerBtn');
 timerBtn.addEventListener("click",function(){
-	console.log(timersc.value + "秒後に〜〜〜〜");
-	setTimeout(setfnc,1000*Number(timersc.value));
+	console.log(timerSc.value + "秒後に〜〜〜〜");
+	setTimeout(setFnc,1000*Number(timerSc.value));
 });
 var img = document.getElementById('bgImageSelect');
-var whiteList = ['http://blog-imgs-42-origin.fc2.com/c/r/a/craft89/130508_01.jpg','http://f.st-hatena.com/images/fotolife/p/pema/20130529/20130529144433.jpg','http://blogs.c.yimg.jp/res/blog-de-53/komeijisatorichireiden/folder/513683/68/10272268/img_0?1377428661']
+var imgList = ['http://blog-imgs-42-origin.fc2.com/c/r/a/craft89/130508_01.jpg','http://f.st-hatena.com/images/fotolife/p/pema/20130529/20130529144433.jpg','http://blogs.c.yimg.jp/res/blog-de-53/komeijisatorichireiden/folder/513683/68/10272268/img_0?1377428661']
 img.addEventListener('change',function(){
-	document.body.style.background = 'url("' + whiteList[img.value] + '")'
+	document.body.style.background = 'url("' + img[img.value] + '")'
 
 })
