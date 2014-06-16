67c986d006c36fb645b04fc8150a94dee4f1c7e9
nonylene <cookie.speaker@gmail.com>
2014-06-16 22:45:24 +0900

    twitter？

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-var welcome = document.getElementById('welcome');
-welcome.addEventListener("click",function(){
-	alert('welcome');
-});
-var setFnc = function(){
-	alert(timerSc.value + "びょう経ちました")
-};
-var timerSc = document.getElementById('timerMSec');
-var timerBtn = document.getElementById('timerBtn');
-timerBtn.addEventListener("click",function(){
-	console.log(timerSc.value + "秒後に〜〜〜〜");
-	setTimeout(setFnc,1000*Number(timerSc.value));
-});
-var img = document.getElementById('bgImageSelect');
-var imgList = ['http://blog-imgs-42-origin.fc2.com/c/r/a/craft89/130508_01.jpg','http://f.st-hatena.com/images/fotolife/p/pema/20130529/20130529144433.jpg','http://blogs.c.yimg.jp/res/blog-de-53/komeijisatorichireiden/folder/513683/68/10272268/img_0?1377428661']
-img.addEventListener('change',function(){
-	document.body.style.background = 'url("' + img[img.value] + '")'
+$('#name').on('click',function(){
+	if ( $('#profile').css('display') === 'none' ){
+		$('#profile').css('display','block');
+		}else{
+		$('#profile').css('display','none');
+	}})
 
+n = 0
+
+$('#welcome').on('click',function(){
+	$('#profile').append('<h2>うごいているよ</h2>\n<input type="button" id="button' + n + '" value="んんん">')
+	n = n + 1
 })
+
+var test = function(d,s,id){
+	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+	if(!d.getElementById(id)){
+		js=d.createElemen    t(s);
+		js.id=id;
+		js.src=p+'://platform.twitter.com/widgets.js';
+		fjs.parentNode.insertBefore(js,fjs);
+		}}
+
+document.getElementById('twitter').addEventListener('click',test(document,'script','twitter-wjs'))
