3b82fc7dc9d195e9bbeffc5fd2fbdc7ea8284cb0
nonylene <cookie.speaker@gmail.com>
2014-06-17 20:44:19 +0900

    取得

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,3 +1,23 @@
 $(function(){
-  
+$('#start').on('click',function(){start();setTimeout(function(){console.log(session)},1000)})
+
+var session = null;
+
+function start(){
+	$.ajax({
+		url: 'http://2048.semantics3.com/hi/start/json',
+		dataType: 'json',
+		data: {},
+		method: 'GET'
+	}).done(function(data){
+		for (var i=0; i<4 ; i++){
+			$('#2048').append(data['grid'][i] + "<br/>");
+		}
+		session = data['session_id']
+	})
+}
+
 })
+
+
+
